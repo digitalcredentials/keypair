957ad1cb2d7608df0b28d5f97e38769246d57697
Dmitri Zagidulin <dzagidulin@gmail.com>
2025-04-30 14:09:52 -0400

    Release v2.0.5
    Signed-off-by: Dmitri Zagidulin <dzagidulin@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 /*!
  * Copyright (c) 2022 Digital Credentials Consortium. All rights reserved.
  */
-export { KeyPair, IVerificationResult } from './KeyPair'
+export { KeyPair, IVerificationResult } from './KeyPair.js'
